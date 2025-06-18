3044ae052e3ae30f084d01197b6dedf77851c0f7
L0dyv <170872410+L0dyv@users.noreply.github.com>
2025-06-18 16:44:37 +0800

    First available version

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -14,6 +14,12 @@ export default defineConfig(({ mode }) => ({
     react(),
     mode === 'development' &&
     componentTagger(),
+    viteStaticCopy({
+      targets: [
+        { src: 'manifest.json', dest: '.' },
+        { src: 'public/favicon.ico', dest: '.' }
+      ]
+    })
   ].filter(Boolean),
   resolve: {
     alias: {
